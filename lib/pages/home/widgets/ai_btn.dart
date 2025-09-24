3f33addea56ae03116ffe793f12a904e57e32919
moong414 <moongom414@gmail.com>
2025-09-24 17:16:18 +0900

    UI: 텍스트 버튼 등 수정

--- lib/pages/home/widgets/ai_btn.dart
+++ lib/pages/home/widgets/ai_btn.dart
@@ -44,7 +44,7 @@ class _AiBtnState extends State<AiBtn> {
               children: [
                 Text(
                   '여행계획 세우기 귀찮을 땐?',
-                  style: AppTxtSt.txtStL.copyWith(fontWeight: FontWeight.w300),
+                  style: AppTxtSt.txtStL,
                 ),
                 SizedBox(height: 16),
                 Container(
